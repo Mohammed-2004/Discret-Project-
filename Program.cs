acf4ccf36b2fbb1d0870b07532090f18894fcced
Mohammed-2004 <111666595+Mohammed-2004@users.noreply.github.com>
2022-12-30 04:23:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discrete_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+   
+
+            Console.Write(" Enter The first Number :  ");
+            int firstnum = int.Parse(Console.ReadLine());       
+
+            Console.Write(" Enter The Last Number :  ");
+            int lastnum = int.Parse(Console.ReadLine());       
+
+            for (int num = firstnum; num <= lastnum; num++)   
+            {
+                int c = 0;                                  
+                for (int k = 2; k <= num/2; k++)           
+                {
+                    if (num % k == 0)                      
+                    {                                     
+                        c++;                            
+                       break;                           
+                    }                                       
+                } 
+                if (c == 0 && num != 1 && num != 0)
+                { 
+                  Console.WriteLine(num);          
+                }                                   
+            }
+                                                  
+            Console.WriteLine("\n");
+
+
+
+
+        }   
+
+    }
+}
